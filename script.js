39b6364e20896a2a8fd66c89986f755e48b1b578
Alejandro Pedreros <pedrerosalejandro_21@hotmail.com>
2022-12-05 21:07:08 -0600

    updated faultyItems id element abd created an Event Listener

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
-// Write your JavaScript code here!
+// const { formSubmission } = require("./scriptHelper");
 
-window.addEventListener("load", function() {
-
+   window.addEventListener("load", function() {
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+       pickPlanet(planets);
    })
-   
+
+
+    list = document.getElementById("faultyItems");
+    //list.style.visibility = "hidden"
+    let form = document.querySelector("form");
+    // console.log(form)
+    form.addEventListener('submit', function(event) {
+    // console.log('Submit')
+    
+        let pilotNameInput = document.querySelector("input[name=pilotName]");
+        let pilot = pilotNameInput.value;
+        let copilotNameInput = document.querySelector("input[name=copilotName]");
+        let copilot = copilotNameInput.value;
+        let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+        let fuelLevel = Number(fuelLevelInput.value);
+        let cargoMassInput = document.querySelector("input[name=cargoMass]");
+        let cargoLevel = Number(cargoMassInput.value);
+        event.preventDefault();
+
+    // console.log(typeof formSubmission)
+
+        formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
+        
+    });
 });
